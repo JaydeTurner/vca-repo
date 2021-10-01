232b31c4a76e5930f3e8380dd6237a9edea4b576
Jayde <Jaydeturner92@gmail.com>
2021-10-01 21:03:56 +0100

    Attempt at creating endpoints

--- test.js
+++ test.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
     res.send('Hello Virtual Machine!')
 })
 
+app.post('/post', function (req, res) {
+    res.send('Got a POST request')
+})
+
+app.put('/put', function (req, res) {
+    res.send('Got a PUT request at /user')
+})
+
+app.delete('/delete', function (req, res) {
+    res.send('Got a DELETE request at /user')
+})
+
 app.get('/servicea', (req, res) => {
     res.send('Hello Virtual Machine! - This is Service A')
 })
